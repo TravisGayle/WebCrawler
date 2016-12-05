d92930dbb42d425dc4ce7b3df34e08a7d96ec5fb
Travis Gayle <TravisGayle@Traviss-MacBook-Air.local>
2016-12-05 20:27:38 +0000

    updated website, basic format

--- web/js/main.js
+++ web/js/main.js
@@ -2,4 +2,21 @@
 
 $(document).ready(function(){
 	//Put code here
+
+	$("#urlButton").click(function() {
+		Hunt();
+	});
+
+	function Hunt(){
+
+		var url = document.getElementById('url1');
+
+		if( url.value){
+			alert("The following url was entered: " + url.value);
+		}else{
+			alert("Error: No URL entered");
+		}
+
+	}
+
 });
