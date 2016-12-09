57c2262e9acaa849bda4e11bc2107d898769c801
lbrenna4 <lbrenna4@nd.edu>
2016-12-09 16:23:49 -0500

    love

--- web/js/main.js
+++ web/js/main.js
@@ -6,59 +6,59 @@ $(document).ready(function(){
 	//Put code here
 
 	$("#urlButton").click(function() {
-		Hunt();	
+		Hunt();
 	});
 		
-})
-	function Hunt(){
+});
 
-		var url = document.getElementById('url1');
-		var maxPage = document.getElementById('maxPage1');
-		if( isNaN(maxPage.value)){
-			alert("ERROR: " + maxPage.value + " is not a number!");
-		}
-		var adjList = httpGetAsync(
-			"http://student00.cse.nd.edu:9001/post", 
+function Hunt(){
+	var url = document.getElementById('url1');
+	var maxPage = document.getElementById('maxPage1');
+	if( isNaN(maxPage.value)){
+		alert("ERROR: " + maxPage.value + " is not a number!");
+	}
+	adjList= httpGetAsync(
 			url.value, 
 			maxPage.value);		
-//		graphMe(url.value);
+		graphMe(url.value);
 //		adjList_to_nodeEdge(adjList);
-	}
+}
 
-	function httpGetAsync(url, wikipage, maxPages){
-		var xhr = new XMLHttpRequest();
-		var response = [];
-		var data;
-		response.push(wikipage + ' ' +  maxPages);
-		xhr.open("POST", url, true);
-		xhr.send(response.join('\n'));
-		xhr.onreadystatechange = function()	{
-			if (xhr.readyState == 4 && xhr.status == 200){
-				data = JSON.parse(xhr.responseText);
-				callback(data);
-				console.log(data);
-			}
+function httpGetAsync(url, wikipage, maxPages){
+	console.log("new xhr");
+	var xhr = new XMLHttpRequest();
+	var response = [];
+	var data;
+	response.push(wikipage + ' ' +  maxPages);
+	xhr.open("POST", url, true);
+	xhr.send(response.join('\n'));
+	xhr.onreadystatechange = function()	{
+		if (xhr.readyState == 4 && xhr.status == 200){
+			data = JSON.parse(xhr.responseText);
+			callback(data);
+			console.log(data);
 		}
-		return data;
 	}
+	return data;
+}
 
-	function callback(text){
-		document.getElementById("Bottom").innerHTML = text;
+function callback(text){
+	document.getElementById("Bottom").innerHTML = text;
+}
+	
+function adjList_to_nodeEdge(data){
+	var graph = {
+		nodes: [],
+		edges: []
 	}
-/*	
-	function adjList_to_nodeEdge(data){
-		var graph = {
-			nodes: [],
-			edges: []
+	for(var key in data){
+		if (!data.hasOwnProperty(key)){
+			continue;
 		}
-		for(var key in data){
-			if (!data.hasOwnProperty(key)){
-				continue;
-			}
-			console.log( key + " -> " + data[key]);
-			//graph['nodes'].append{ id: 'key'}
-			//edges
-		}
+		console.log( key + " -> " + data[key]);
+		//graph['nodes'].append{ id: 'key'}
+		//edges
 	}
-*/
+}
+
 
